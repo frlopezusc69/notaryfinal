2655fa4506299644050908f542e0a32550bf24b2
Felix Lopez <81645633+frlopezusc69@users.noreply.github.com>
2021-06-15 19:41:02 -0700

    more edits

--- React-FrontEnd/notary/src/Components/navbar.js
+++ React-FrontEnd/notary/src/Components/navbar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
 
 function Navbar() {
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
--- React-FrontEnd/notary/src/index.js
+++ React-FrontEnd/notary/src/index.js
@@ -3,16 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom';
+import Navbar from './navbar';
+import Footer from './footer';
 
-ReactDOM.render(
-  <React.StrictMode>
-      <Router>
-    <App />
-      </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const routing = (
+  <Router>
+    <div>
+      <Navbar />
+      <hr />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+      <Footer />
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
